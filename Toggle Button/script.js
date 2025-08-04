365ff42d7c15e14bfed9536ba8fb7a4f6b9b88da
Hari Thapa <harithapa4654@gmail.com>
2025-08-04 14:46:34 +0530

    toggle button

---
+++ Toggle Button/script.js
@@ -0,0 +1,18 @@
+const toggle = document.getElementById('toggleDark')
+const body = document.querySelector('body')
+
+toggle.addEventListener('click' , function () {
+    this.classList.toggle('fa-moon')
+        if (this.classList.toggle('fa-sun')) {
+            body.style.background = 'white';
+            body.style.color = 'red';
+            body.style.transition = '2s';
+        }
+
+        else {
+            body.style.background = 'black';
+            body.style.color = 'white';
+            body.style.transition = '2s';
+        }
+
+})
