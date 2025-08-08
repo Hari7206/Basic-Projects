f5632001461458f93fb94ce6d337553b8a81da23
Hari Thapa <harithapa4654@gmail.com>
2025-08-08 15:42:17 +0530

    bmi calculator

---
+++ bmicalculator/script.js
@@ -0,0 +1,22 @@
+
+let btn = document.querySelector('#btn')
+let reset = document.querySelector('.btn')
+let result = document.querySelector('#normal')
+
+btn.addEventListener('click' , () => {
+    let height = Number(document.querySelector("#heightInput").value);
+    let weight = Number(document.querySelector("#weightInput").value);
+        if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+        result.innerHTML = "Invalid input. Please enter valid height and weight.";
+        return; // Stop further execution
+    }
+     height = height / 100;
+   let  BMI = weight  / (height * height)
+    BMI = BMI.toFixed(2); // round to 2 decimal places
+result.innerHTML = `Your BMI is ${BMI}`;
+
+})
+
+reset.addEventListener('click', () => {
+    location.reload();
+});
