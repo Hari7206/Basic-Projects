71965619914da7a78d59ed17ccee8d16bbbcc328
Hari Thapa <harithapa4654@gmail.com>
2025-08-11 15:15:42 +0530

    counter app

---
+++ calculatos/counter/script.js
@@ -0,0 +1,14 @@
+let result = document.querySelector('.result');
+let incre = document.querySelector('.incre')
+let decre = document.querySelector('.decre')
+let reset = document.querySelector('.reset')
+
+incre.addEventListener('click' , () =>{
+    return +1;
+})
+decre.addEventListener('click' , () =>{
+    return +1;
+})
+
+
+result.innerHTML = `${incre}`
